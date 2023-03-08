0445f4d535da43c75c0b08d6879d43e3c02dbd61
Cecilia Junqueira Sartini <sartinicj@outlook.com>
2023-03-08 15:45:44 -0300

    Creating a multiple table

---
+++ Python/tabuada.py
@@ -0,0 +1,12 @@
+num = int(input('Escolha um numero: '))
+i = 0
+while (i<=10):
+    prd = num*i
+    print(' ', i, 'x', num, '=', prd)
+    i=i+1
+else:
+    print('Fim da tabuada do',num)
+    
+      #TODO: 
+        # na segunda vez que mostrar o resultado, perguntar se quer fazer uma nova tabuada, ou encerrar
+
