f3a09940f4e13f8afe64ef123a488ecac2aa9f2e
Cecilia Junqueira Sartini <sartinicj@outlook.com>
2024-03-28 07:44:46 -0300

    [ADD]List exercise
    Started the exercise of list manipulation

--- Python/90-aula54-exercicio-lista-de-compras.py
+++ Python/90-aula54-exercicio-lista-de-compras.py
@@ -1,14 +1,30 @@
 """
-Faça uma lista de compras com listas
-O usuario deve ter a possibilidade de inserir e apagar valores
+Crie um menu de opções para a criação de uma lista de compras
+O usuario deve ter a possibilidade de inserir, editar e apagar valores
 Não permita que o prigrama quebre com erros de indices inexistentes
 """
 
-compras_mercado = ['Vinagre', 'Pasta de dente']
-
-for item in enumerate(compras_mercado):
-    print('Itens da lista: ', item)
+lista = []
+entrada = True
+while entrada == False:
+    print('Lista de compras!')
+    print('1 para inserir um item')
+    print('2 para editar um item existente')
+    print('3 para apagar um item')
+    print('4 para visualizar sua item')
+    print('5 para sair')
+    escolha = int(input('Qual a opão desejada: '))
     
-for tupla_enumerada in enumerate(compras_mercado):
-    for valor in tupla_enumerada:
-        print('Item da lista: ',valor)
+    if escolha == '1':
+        inserir = input('O que deseja incluir? ')
+        lista.append(inserir)
+    
+    if escolha == '2':
+        lista = tuple(lista)
+        for item in enumerate:
+            print('Sua lista atua atual está assim: ', item)
+            editar = input('Qual item deseja alterar? ')
+            for editar in lista:
+                adicionar = input('Informe o novo item: ')
+                lista = list(lista)
+                lista.append 
